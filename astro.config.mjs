aa2ed2d0aabd085158e7abd299f4f74aca85fedb
Kiracoon <kiramyby@gmail.com>
2025-07-08 21:54:51 +0800

    config init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: 'night-owl',
+      wrap: true
+    },
+  },
+  integrations: [vue({ devtools: true })],
+});
