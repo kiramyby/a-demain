353874a345c014ac7d25670dfa73b44ba80dbccf
Kiracoon <kiramyby@gmail.com>
2026-01-28 18:41:25 +0800

    style: fmt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
-import icon from 'astro-icon';
-
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: 'night-owl',
-      wrap: true
-    },
-  },
+	markdown: {
+		shikiConfig: {
+			theme: "night-owl",
+			wrap: true,
+		},
+	},
 
-  integrations: [react(), icon()],
+	integrations: [react(), icon()],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
