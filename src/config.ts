f2e749ff2a499c068765173985cd01c133add121
Kiracoon <kiramyby@gmail.com>
2026-01-28 19:02:31 +0800

    feat: use config.ts managing content

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const SITE_CONFIG = {
+	author: {
+		name: "Kira Kiraco0n",
+		bio: "Web Dev | Cyber Security | Noob Writer",
+		email: "kiramyby@gmail.com",
+		avatarUrl:
+			"https://gravatar.com/avatar/82c042f60d38e82d8084e8c100c4122e887bfc6960a8e28deb0f85e1e10b7f90?size=256",
+	},
+	social: {
+		github: "https://github.com/kiramyby",
+		x: "https://x.com/kir4_c4",
+	},
+};
